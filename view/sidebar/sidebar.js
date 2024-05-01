b2af16ad036b03260a5208a9ba90af22e89627d1
unknown <tubinh10102004@gmail.com>
2024-05-01 10:36:09 +0700

    tách folder

--- view/sidebar/sidebar.js
+++ view/sidebar/sidebar.js
@@ -1,22 +1,22 @@
 function hienThiMonAnTheoCategory(category) {
   switch (category) {
     case 'category_ga':
-      window.location.href = 'menu_ga.html';
+      window.location.href = '../menu/menu_ga.html';
       break;
     case 'category_combo':
-      window.location.href = 'menu_combotietkiem.html';
+      window.location.href = '../menu/menu_combotietkiem.html';
       break;
     case 'category_burger':
-      window.location.href = 'menu_burger.html';
+      window.location.href = '../menu/menu_burger.html';
       break;
     case 'category_snack':
-      window.location.href = 'menu_cacmonankem.html';
+      window.location.href = '../menu/menu_cacmonankem.html';
       break;
     case 'category_Noodle':
-      window.location.href = 'menu_myy.html';
+      window.location.href = '../menu/menu_myy.html';
       break;
     case 'category_drink':
-      window.location.href = 'menu_nuocgiaikhat.html';
+      window.location.href = '../menu/menu_nuocgiaikhat.html';
       break;
     default:
       break;
