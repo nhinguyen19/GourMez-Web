693626b9f0110c4b65d0346ea536d12cdf7fe2a7
tubinguyen <tubinh10102004@gmail.com>
2024-05-03 17:46:50 +0700

    tao xong trang hien thi và sidebar ở admin

--- view/admin/tranghienthi.php
+++ view/admin/tranghienthi.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADMINCP</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to AdminCP</title>
+  <link rel="stylesheet" href="sidebar_ad/sidebar_ad.css">
 </head>
 <body>
-    <h3>Welcom to AdminCP </h3>
-    <?php
-        include("header_ad.php");
-        include("footer_ad.php");
-        include("sidebar_ad/sidebar_ad.html");
-    ?>
+  <h3>Welcome to AdminCP</h3>
+
+  <?php
+    include("header_ad.php");
+    include("footer_ad.php");
+    include("sidebar_ad/sidebar_ad.html");
+  ?>
+
 </body>
 </html>
