e9edba3f586ccab1f1f1cafd78055e1fed810c4b
minhanhnb <121483421+minhanhnb@users.noreply.github.com>
2024-05-03 12:41:21 +0700

    admin

---
+++ admin/view/pages/sidebar.php
@@ -0,0 +1,13 @@
+
+
+<div class="sidebar">
+       <ul class="list_sidebar">
+         <li><a href="index.php?act=danhmuc&id=1"> Thêm danh mục</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=2"> Combo tiết kiệm</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=3"> Combo gia đình</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=4"> Burger</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=5"> Các món ăn kèm</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=6"> Mì ý</a></li>
+         <li><a href="index.php?act=danhmucthucdon&id=7"> Nước giải khát</a></li>
+        </ul>
+</div>
