df91138e28ca31b72a4bef9cfa7317898a01318c
dynnyd <duongyennhi270904@gmail.com>
2024-05-10 22:33:52 +0700

    dyn

--- view/cus/header/header_sauDN.php
+++ view/cus/header/header_sauDN.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
-    <link rel="stylesheet" href="header_sauDN.css">
+    <link rel="stylesheet" href="./header.css">
     
     <link href='https://fonts.googleapis.com/css?family=Lalezar' rel='stylesheet'>
     <style>
@@ -23,14 +23,14 @@
         <!-- <div id="img"><img src="C:\xampp\htdocs\DoAn\Helene.png" style="height: 86.61px; width: 100px;"></div> -->
         
         <ul class="menu_header">
-            <li><a href="#" id="header"><img src="view_dyn/img/Helene.png" style="height: 150px; width: 150px;"></a></li>
-            <li><a href="#" id="header">THỰC ĐƠN</a></li>
-            <li><a href="#" id="header">KHUYẾN MÃI</a></li>
-            <li><a href="#" id="header">DỊCH VỤ</a></li>
-            <li><a href="#" id="header">TIN TỨC</a></li>
-            <li><a href="#" id="header">LIÊN HỆ</a></li>
-            <li><a href="#" id="header">VỀ CHÚNG TÔI</a></li>
-            <li><a href="#" id="header"><i class="fas fa-shopping-cart"></i></a></li>
+            <li><a href="tranghienthi.php?quanly=trangchu&id=1" id="header"><img src="../img/logocus.png" style="height: 150px; width: 150px;"></a></li>
+            <li><a href="tranghienthi.php?quanly=thucdon" id="header">THỰC ĐƠN</a></li>
+            <li><a href="tranghienthi.php?quanly=khuyenmai" id="header">KHUYẾN MÃI</a></li>
+            <li><a href="tranghienthi.php?quanly=dichvu" id="header">DỊCH VỤ</a></li>
+            <li><a href="tranghienthi.php?quanly=tintuc" id="header">TIN TỨC</a></li>
+            <li><a href="tranghienthi.php?quanly=lienhe" id="header">LIÊN HỆ</a></li>
+            <li><a href="tranghienthi.php?quanly=vechungtoi" id="header">VỀ CHÚNG TÔI</a></li>
+            <li><a href="tranghienthi.php?quanly=giohang" id="header"><i class="fas fa-shopping-cart"></i></a></li>
         </ul>
 
         <div class="user_dropdown" > 
