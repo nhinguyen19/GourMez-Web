35aa207d5f0001d8e08b54849ec64cbf6cd08464
minhanhnb <121483421+minhanhnb@users.noreply.github.com>
2024-05-12 21:44:35 +0700

    update

--- view/cus/trangchu/trangchu.php
+++ view/cus/trangchu/trangchu.php
@@ -114,6 +114,7 @@ function renderCity(data) {
   };
 }
 	</script>
+  <img class="postertrangchu" src="../view/cus/img/postertrangchu.png">
             </script>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
