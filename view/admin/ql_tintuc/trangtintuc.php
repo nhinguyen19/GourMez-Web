487acd8fbd006028846fb3b5168702d49880b151
daylaLili <phmhongchu@gmail.com>
2024-05-16 23:11:17 +0700

    them trang tin tuc

---
+++ view/admin/ql_tintuc/trangtintuc.php
@@ -0,0 +1,54 @@
+<link href='https://fonts.googleapis.com/css?family=Lalezar' rel='stylesheet'>
+<link rel="stylesheet" href="trangtintuc.css">
+
+<div id="container" style="background-color: #feeaca">
+    <div>
+        <img id="img_header" src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="opacity: 0.5;  filter: alpha(opacity=50)">
+        <img src="../../admin/img/logoadmin.png">
+        <h2>Tin tức</h2>
+        <button id="thaydoi" name="subject" type="submit" value="CSS" style="background-color: #159C33; color:azure; border-radius: 5px; border:none" >Thay dổi</button>
+        <div>
+            <table>
+                <tr>
+                    <td style="width: 30 px; margin:0">
+                        <img src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="width:70%; height:70%"> <br>
+                        <a
+                            href="https://jollibee.com.vn/blog/post/jollibee-%E2%80%93-th%E1%BA%A5u-hi%E1%BB%83u-d%E1%BB%83-thanh-cong-duplicated">Tham
+                            vọng trở thành thương hiệu yêu thích của giới trẻ</a>
+                        <p>Từ một dự án web được đầu tư chi phí ít ỏi mà giờ đây...</p>
+                        <a
+                            href="https://jollibee.com.vn/blog/post/jollibee-%E2%80%93-th%E1%BA%A5u-hi%E1%BB%83u-d%E1%BB%83-thanh-cong-duplicated">Xem
+                            chi tiết</a>
+                        <button id="chinhsua" name="subject" type="submit" value="CSS">Chỉnh sửa</button>
+                    </td>
+                    <td>
+                        <div>
+                            <img src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="width:30%; height:30%"><br>
+                            <a href="https://jollibee.com.vn/blog/post/jollibee-tham-v%E1%BB%8Dng-tr%E1%BB%9F-thanh-thuong-hi%E1%BB%87u-fast-food-du%E1%BB%A3c-yeu-thich-nh%E1%BA%A5t-vi%E1%BB%87t-nam-duplicated">Gourméz
+                                tiếp tục phát triển mạnh sau giãn cách</a> 
+                            <p>Sau giãn cách nhờ những đúng đắn...</p>
+                            <button id="chinhsua" name="subject" type="submit" value="CSS">Chỉnh sửa</button>
+                        </div>
+                        <div>
+                            <img src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="width:30%; height:30%"> <br>
+                            <a href="https://jollibee.com.vn/blog/post/jollibee-%E2%80%93-th%E1%BA%A5u-hi%E1%BB%83u-d%E1%BB%83-thanh-cong-duplicated">Giao
+                                hàng nhanh và tiếp kiệm</a> 
+                                
+                            <p>Luôn ưu tiên trải nghiệm của khách hàng...</p>
+                            <button id="chinhsua" name="subject" type="submit" value="CSS">Chỉnh sửa</button>
+                        </div>
+                        <div>
+                            <img src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="width:30%; height:30%"> <br>
+                            <a  href="https://jollibee.com.vn/blog/post/jollibee-%E2%80%93-th%E1%BA%A5u-hi%E1%BB%83u-d%E1%BB%83-thanh-cong-duplicated">Nền
+                                tảng hình thành của Gourméz</a> 
+                            <p>Hành động nhỏ mang lại lợi ích to lớn. Ngày 17/4 bà Nguyễn Ngọc...</p>
+                            <button id="chinhsua" name="subject" type="submit" value="CSS">Chỉnh sửa</button>
+                        </div>
+                    </td>
+                </tr>
+                <div>
+                    <button id="them" name="subject" type="submit" value="CSS">Thêm bài viết</button>
+                </div>
+            </table>
+        </div>
+    </div>
