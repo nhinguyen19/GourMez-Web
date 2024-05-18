379e0888b4ba5bbd752713b4f68e3a6397d24cf6
daylaLili <phmhongchu@gmail.com>
2024-05-18 15:02:22 +0700

    them upload ảnh tin tức

--- view/cus/vechungtoi/vechungtoi.php
+++ view/cus/vechungtoi/vechungtoi.php
@@ -1,4 +1,53 @@
 <link href='https://fonts.googleapis.com/css?family=Lalezar' rel='stylesheet'>
-<div id="container">
-    <img src="">
+<div id="container" style="background-color: #feeaca">
+    <div id="header" style="background-color:white">
+        <h1>Giới thiệu về chúng tôi</h1>
+        <img src="../../cus/img/logocus.png">
+
+    </div>
+    <div id="gioithieu">
+        <table>
+            <tr>
+                <td style="background-color: #AE2108; box-shadow:10px 10px 10px grey">
+                    <img src="../../cus/img/logocus.png">
+                </td>
+                <td>
+                    <h2 style="size:50px">Một dự án web đặt món ăn tên Gourméz </h2>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div id="giatri">
+        <img src="../../admin/ql_tintuc/img_tintuc/anhchotintuc.png" style="opacity: 0.5;  filter: alpha(opacity=50)">
+        <div>
+            <h2 style="color:white">Các giá trị</h2>
+            <table>
+                <tr>
+                    <td>
+                        <img src="../../cus/vechungtoi/img_vechungtoi/nguoi.png">
+                        <p style="color:white">Khách hàng là trọng tâm</p>
+                    </td>
+                    <td>
+                        <img src="../../cus/vechungtoi/img_vechungtoi/timlike.png">
+                        <p style="color:white">Đề cao sự tôn trọng đối với cá nhân</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="../../cus/vechungtoi/img_vechungtoi/kienthuc.png">
+                        <p style="color:white">Khiêm tốn lắng nghe và học hỏi</p>
+                    </td>
+                    <td>
+                        <img src="../../cus/vechungtoi/img_vechungtoi/tietkiem.png">
+                        <p style="color:white">Tiết kiệm</p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+    <div>
+    
+    </div>
+
 </div>
