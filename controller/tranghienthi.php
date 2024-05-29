39a761b9e66d68126833ad7491f694ec00eba520
dynnyd <duongyennhi270904@gmail.com>
2024-05-29 23:28:03 +0700

    dyn

--- controller/tranghienthi.php
+++ controller/tranghienthi.php
@@ -1,17 +1,10 @@
 <?php
 
     include("xuly_tinnhanKH.php");
+    include("xuly_thanhtoan.php");
     session_start();
     include("../view/cus/header/header.php");
-    
-=======
-    include("xuly_tinnhanKH.php");
-    session_start();
-    if(isset($_SESSION['role']) && ($_SESSION['role'] == 0)) {
-        include("../view/cus/header/header_sauDN.php");
-    } else {
-        include("../view/cus/header/header_trcDN.php");
-    }
+
 ?>
 <?php 
 
@@ -111,4 +104,7 @@
 
 ?>
 
->>>>>>> bd521305dda4d75cc0a0805b74acc24c3fe5f9c4
+
+<?php
+    include("../view/cus/footer/footer.php");
+?>
