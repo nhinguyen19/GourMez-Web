80be844d9bf532e52f1a51c91c8e3b55e772e112
minhanhnb <121483421+minhanhnb@users.noreply.github.com>
2024-06-05 22:42:46 +0700

    update

--- view/cus/giohang/giohang.php
+++ view/cus/giohang/giohang.php
@@ -29,7 +29,6 @@
 $conn = connectdb();
 $user_id = isset($_SESSION['id']) ? mysqli_real_escape_string($conn, $_SESSION['id']) : null;
 
-
 if (isset($_POST['update'])) {
     $foodId = $_POST['food_id'];
     $quantity = $_POST['quantity'];
@@ -38,6 +37,7 @@ if (isset($_POST['update'])) {
     if ($user_id) {
         $updateQuantityQuery = "UPDATE cart SET quantity = $quantity WHERE food_id = $foodId AND user_id = $user_id";
         mysqli_query($conn, $updateQuantityQuery);
+        $isUpdated = true;
     } else {
         for ($i = 0; $i < sizeof($_SESSION['cart']); $i++) {
             if ($_SESSION['cart'][$i]['food_id'] == $foodId) {
